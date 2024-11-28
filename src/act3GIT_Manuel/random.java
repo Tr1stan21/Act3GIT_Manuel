1a2a945477cfcc5028103666defc970e610e0b6b
Manuel Álvarez Camacho <malvcam2409@g.educaand.es>
2024-11-28 09:38:15 +0100

    añado el 7 mas 7

--- src/act3GIT_Manuel/random.java
+++ src/act3GIT_Manuel/random.java
@@ -1,15 +1,52 @@
 package act3GIT_Manuel;
 
 import java.util.Random;
+import java.util.Scanner;
 
-public class random {
-	
-	 public static void main(String[] args) {
-	        Random random = new Random();
-	        
-	        for (int i = 0; i < 20; i++) {
-	            int numeroAleatorio = random.nextInt(10) + 1;
-	            System.out.println(numeroAleatorio);
-	        }
-	    }
+public class random {  
+    
+    public static void main(String[] args) {
+        Random random = new Random();
+        
+       
+        for (int i = 0; i < 20; i++) {
+            int numeroAleatorio = random.nextInt(10) + 1;
+            System.out.println(numeroAleatorio);
+        }
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        int num1 = 0, num2 = 0;
+      
+        while (true) {
+            System.out.print("Introduce el primer número: ");
+            num1 = scanner.nextInt();
+            System.out.print("Introduce el segundo número: ");
+            num2 = scanner.nextInt();
+
+            if (num1 != num2) {
+                break;
+            } else {
+                System.out.println("Los números deben ser distintos. Inténtalo de nuevo.");
+            }
+        }
+        
+       
+        imprimirNumerosEntre(num1, num2);
+
+        scanner.close();
+    }
+    
+    
+    public static void imprimirNumerosEntre(int a, int b) {
+        if (a > b) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        for (int i = a; i < b; i += 7) {
+            System.out.println(i);
+        }
+    }
 }
