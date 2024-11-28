de03977eb7b5cbe6427470673e9e3263a5cab98d
Manuel Álvarez Camacho <malvcam2409@g.educaand.es>
2024-11-28 09:25:14 +0100

    gitignore

--- src/act3GIT_Manuel/random.java
+++ src/act3GIT_Manuel/random.java
@@ -2,14 +2,19 @@ package act3GIT_Manuel;
 
 import java.util.Random;
 
-public class random {
-	
-	 public static void main(String[] args) {
-	        Random random = new Random();
-	        
-	        for (int i = 0; i < 20; i++) {
-	            int numeroAleatorio = random.nextInt(10) + 1;
-	            System.out.println(numeroAleatorio);
-	        }
-	    }
+public class random {  
+    
+    public static void main(String[] args) {
+        Random random = new Random();
+        
+        
+        for (int i = 0; i < 20; i++) {
+            int numeroAleatorio = random.nextInt(10) + 1;
+            System.out.println(numeroAleatorio);
+        }      
+    }
 }
+
+     
+
+
